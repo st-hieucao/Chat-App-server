1a5d479bcbe91ad963d85f6b9d92cb4424466842
hieu.cao@supremetech.vn <hieu.cao@supremetech.vn>
2022-07-20 21:34:54 +0700

    fix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 // import connectDB from './config/db.js';
 
 const app = express();
-const port = 8080;
 
 // connectDB();
 
@@ -10,6 +11,4 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(port, (req, res) => {
-  console.log(`Example app listening on port ${port}`)
-});
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
