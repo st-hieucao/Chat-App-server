7ea7770b0ce1144c432ae792f18035efab76ed6a
hieu.cao@supremetech.vn <hieu.cao@supremetech.vn>
2022-07-20 21:22:14 +0700

    fix

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 import express from 'express';
-import connectDB from './config/db.js';
+// import connectDB from './config/db.js';
 
 const app = express();
 const port = 8080;
 
-connectDB();
+// connectDB();
 
 app.get('/user', function (req, res) {
   res.send('Hello World')
 })
 
-// app.listen(port, (req, res) => {
-//   console.log(`Example app listening on port ${port}`)
-// });
-
-app.listen(port || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(port, (req, res) => {
+  console.log(`Example app listening on port ${port}`)
 });
