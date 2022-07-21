bb4328b5bcf8391b8f1c5e823cb922b74f5583d6
hieu.cao@supremetech.vn <hieu.cao@supremetech.vn>
2022-07-21 21:55:52 +0700

    handle api user in server

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Hello Cao Kha hieu');
+});
+
 // Router
 app.use("/user", userRouter);
 
